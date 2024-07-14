39eba6febd03556a249041d7fe89ed63071ad8b5
jeffcct <jsmi607@aucklanduni.ac.nz>
2024-07-14 23:05:52 +1200

    uploaded the wave program

--- src/App.js
+++ src/App.js
@@ -10,18 +10,18 @@ function App() {
         
         <h5>View My Profiles:</h5>
         <a href="https://github.com/jeffcct">
-          <img class="icon" src="images/githubIcon.png" alt="" />
+          <img class="icon" src="images/githubIcon.png" alt="GitHub" />
         </a>
         <a href="https://editor.p5js.org/jffrysith/sketches">
-          <img class="icon" src="images/p5jsIcon.png" alt="" />
+          <img class="icon" src="images/p5jsIcon.png" alt="p5.js" />
         </a>
         <a href="www.linkedin.com/in/jffrysith">
-          <img class="icon" src="images/LIIcon.png" alt="" />
+          <img class="icon" src="images/LIIcon.png" alt="LinkedIn" />
         </a>
       </div>
-      <iframe src="https://editor.p5js.org/jffrysith/full/fnySISSqk" width="600px" height="400px"></iframe>
-      <iframe src="https://editor.p5js.org/jffrysith/full/H5tJW2_JQ" width="600px" height="400px"></iframe>
-
+      <iframe src="https://editor.p5js.org/jffrysith/full/fnySISSqk" width="600px" height="400px" title="Flow Field program"></iframe>
+      <iframe src="https://editor.p5js.org/jffrysith/full/H5tJW2_JQ" width="600px" height="400px" title="DBScan clustering algorithm"></iframe>
+      <iframe src="https://editor.p5js.org/jffrysith/full/nWpAy8HGu" width="600px" height="400px" title="Wave Program"></iframe>
     </div>
   );
 }
